b0dae07fd84cd7818303be5e6ba3656871cfd504
Aleix Ferré <thecatalahd@gmail.com>
2024-12-03 21:31:41 +0100

    Day 3, completed, atleast I think so

--- index.ts
+++ index.ts
@@ -3,10 +3,13 @@ import { day1_2 } from "./day-1/day1-2";
 import { day2 } from "./day-2/day2";
 import { day2_2 } from "./day-2/day2-2";
 import { day2_2_optimized } from "./day-2/day2-2_optimized";
+import { day3 } from "./day-3/day3";
+import { day3_2 } from "./day-3/day3-2";
 
 const daysFunctions: Function[][] = [
   [day1, day1_2],
   [day2, day2_2, day2_2_optimized],
+  [day3, day3_2]
 ];
 
 const dayStr = Bun.env.DAY!;
