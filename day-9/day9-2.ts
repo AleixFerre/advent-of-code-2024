bd1ce7dd867109d235e0000bfe353124a855fbff
Aleix Ferré <thecatalahd@gmail.com>
2024-12-12 23:40:01 +0100

    dia 9-2 per fi dios

--- day-9/day9-2.ts
+++ day-9/day9-2.ts
@@ -5,7 +5,7 @@ export async function day9_2() {
   const values = file.split("").map(p => parseInt(p));
 
   const str: string[] = [];
-  const points: number[] = [];
+  const points: number[][] = [];
 
   for (let i = 0; i < values.length; i++) {
     const element = values[i];
@@ -14,36 +14,65 @@ export async function day9_2() {
         str.push((i / 2).toString())
       }
     } else {
+      const currentPoints = [];
       for (let j = 0; j < element; j++) {
         str.push('.');
-        points.push(str.length - 1);
+        currentPoints.push(str.length - 1);
       }
+      if (currentPoints.length) points.push(currentPoints);
     }
   }
 
-  let pointsIndex = 0;
+  let currentNum = parseInt(str[str.length - 1]);
+
+  let currentStr: string[] = [];
   for (let i = str.length - 1; i >= 0; i--) {
-    if (str[i] === '.') continue;
-    const currentPointIndex = points[pointsIndex];
-    if (currentPointIndex > i) break;
-    swapIndexes(str, i, currentPointIndex);
-    pointsIndex++;
+    const firstChar = currentStr[0];
+    if (str[i] !== firstChar && currentStr.length > 0) {
+      if (firstChar !== '.') {
+        const newNum = parseInt(currentStr[0]);
+        if (newNum <= currentNum) {
+          currentNum = newNum;
+          const firstIndex = findFirstFittedIndex(points, currentStr, i);
+          if (firstIndex !== -1) {
+            swapBlock(currentStr, i, str, points, firstIndex);
+          }
+        }
+      }
+      currentStr = [];
+    }
+    currentStr.push(str[i]);
   }
 
   let total = 0;
 
   for (let i = 0; i < str.length; i++) {
-    const num = str[i];
-    if (num === '.') break;
-    const parsedNum = parseInt(num);
+    if (str[i] === '.') continue;
+    const parsedNum = parseInt(str[i]);
     total += i * parsedNum;
   }
 
   console.log(total);
 }
 
+function swapBlock(currentStr: string[], i: number, str: string[], points: number[][], firstIndex: number) {
+  for (let j = 0; j < currentStr.length; j++) {
+    const swapIndex = i + j + 1;
+    swapIndexes(str, swapIndex, points[firstIndex][j]);
+  }
+  points[firstIndex].splice(0, currentStr.length);
+}
+
 function swapIndexes(str: string[], i1: number, i2: number): void {
   const auxA = str[i1];
   str[i1] = str[i2];
   str[i2] = auxA;
+}
+
+function findFirstFittedIndex(points: number[][], currentStr: string[], maxIndex: number): number {
+  for (let i = 0; i < points.length; i++) {
+    if (points[i][0] > maxIndex) return -1;
+    if (currentStr.length <= points[i].length) return i;
+  }
+  return -1;
 }
