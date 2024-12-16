d8f6ebb4cbe6491e569568a645f0ece3e0b19858
Aleix Ferré <thecatalahd@gmail.com>
2024-12-17 00:32:04 +0100

    dia 15 wip

--- day-15/day15.ts
+++ day-15/day15.ts
@@ -13,10 +13,10 @@ class Vector2 {
 }
 
 const DIRECTIONS: Record<string, Vector2> = {
-  '^': new Vector2(0, -1),  // Top
-  '>': new Vector2(1, 0),   // Right
-  'v': new Vector2(0, 1),   // Bottom
-  '<': new Vector2(-1, 0),  // Left
+  '^': new Vector2(-1, 0),
+  '>': new Vector2(0, 1),
+  'v': new Vector2(1, 0),
+  '<': new Vector2(0, -1),
 }
 
 export async function day15() {
@@ -27,11 +27,48 @@ export async function day15() {
   const maze = parts[0].split('\n').map(p => p.split(''));
   const moves = parts[1].split('').filter(p => p !== '\n');
 
-  const player: Vector2 = findChar(maze, '@');
+  let player: Vector2 = findChar(maze, '@');
 
-  console.table(maze);
-  console.log(moves);
-  console.log(player);
+  for (const move of moves) {
+    const successfully = moveCell(maze, player, DIRECTIONS[move]);
+    if (successfully) player = player.add(DIRECTIONS[move]);
+  }
+
+  let score = 0;
+
+  for (let i = 0; i < maze.length; i++) {
+    for (let j = 0; j < maze[i].length; j++) {
+      const current = maze[i][j];
+      if (current === 'O') {
+        score += 100 * i + j;
+      }
+    }
+  }
+
+  console.log(score);
+
+}
+
+function moveCell(maze: string[][], cellToMove: Vector2, direction: Vector2): boolean {
+  const newPos = cellToMove.add(direction);
+  const charAtNewPos = maze[newPos.x][newPos.y];
+
+  if (charAtNewPos === '#') return false;
+  if (charAtNewPos === '.') {
+    swap(maze, cellToMove, newPos);
+    return true;
+  }
+
+  // Rock
+  const successfully = moveCell(maze, newPos, direction);
+  if (successfully) swap(maze, cellToMove, newPos);
+  return successfully;
+}
+
+function swap(maze: string[][], player: Vector2, newPos: Vector2) {
+  const aux = maze[player.x][player.y];
+  maze[player.x][player.y] = maze[newPos.x][newPos.y];
+  maze[newPos.x][newPos.y] = aux;
 }
 
 function findChar(matrix: string[][], searchChar: string): Vector2 {
